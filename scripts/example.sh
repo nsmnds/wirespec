cab1408e29a4945ed78c19654c12dff261ae4251
Willem Veelenturf <willem.veelenturf@ing.com>
2024-06-23 20:41:31 +0200

    Wip

--- scripts/example.sh
+++ scripts/example.sh
@@ -5,11 +5,11 @@ dir="$(dirname -- "$0")"
 # publish and build gradle example
 ./gradlew src:plugin:gradle:publishToMavenLocal --no-configuration-cache &&
   ./gradlew src:plugin:maven:publishToMavenLocal --no-configuration-cache &&
-  ./gradlew src:plugin:integration:jackson:publishToMavenLocal --no-configuration-cache &&
-  ./gradlew src:plugin:integration:spring:publishToMavenLocal --no-configuration-cache &&
+  ./gradlew src:integration:jackson:publishToMavenLocal --no-configuration-cache &&
+  ./gradlew src:integration:spring:publishToMavenLocal --no-configuration-cache &&
   # build examples
-  (cd "$dir"/../examples/wirespec-gradle-plugin-ktor && ./gradlew clean build) &&
-  (cd "$dir"/../examples/spring-boot-maven-plugin && ./mvnw clean package) &&
-  (cd "$dir"/../examples/spring-boot-custom-maven-plugin && ./mvnw clean package) &&
-  (cd "$dir"/../examples/spring-boot-openapi-maven-plugin && ./mvnw clean package)
+#  (cd "$dir"/../examples/wirespec-gradle-plugin-ktor && ./gradlew clean build) &&
+#  (cd "$dir"/../examples/spring-boot-maven-plugin && ./mvnw clean package) &&
+#  (cd "$dir"/../examples/spring-boot-custom-maven-plugin && ./mvnw clean package) &&
+#  (cd "$dir"/../examples/spring-boot-openapi-maven-plugin && ./mvnw clean package)
   (cd "$dir"/../examples/spring-boot-integration && ./mvnw clean package)
